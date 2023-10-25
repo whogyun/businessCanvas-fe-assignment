0c9b6795d2463118f06291a39a1247b4dcf9c491
Hogyun Woo <ghrbs2070@gmail.com>
2023-10-25 20:57:43 +0900

    라이브러리 추가 & 불필요 파일 및 코드 제거

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,28 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 13,
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint'],
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'airbnb',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
     ],
-  },
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    rules: {
+        '@typescript-eslint/interface-name-prefix': 'on',
+        '@typescript-eslint/explicit-function-return-type': 'on',
+        '@typescript-eslint/explicit-module-boundary-types': 'on',
+        '@typescript-eslint/no-explicit-any': 'on',
+    },
 }
